b141976a1435eaec1bd6c570fe0feaabcfbc8284
Isabella <isabella.y.hu@gmail.com>
2022-04-13 22:42:23 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const pets = ['Milo','Otis','Garfield'];
+
+const append = [...pets];
+append.push('Odie');
+
+const prepend = [...pets];
+prepend.unshift('Odie');
+
+const removeLast = [...pets];
+removeLast.pop();
+
+const removeFirst = [...pets];
+removeFirst.shift();
